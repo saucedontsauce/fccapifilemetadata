abc7b7dd971be317deb47fde5911573d09e867ed
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2024-02-28 18:28:50 +0000

    finished?

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
+const bodyParser = require('body-parser');
+const multer = require('multer')
+const upload = multer({ dest: "/uploads" })
+
 
 var app = express();
+app.use(express.json())
+app.use(bodyParser.urlencoded({ "extended": true }))
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -10,6 +17,23 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post('/api/fileanalyse',upload.single("upfile"), (req, res) => {
+  let rdata = {
+    "name": "",
+    "type": "",
+    "size": ""
+  }
+  if (req.file) {
+    rdata.name = req.file.originalname
+    rdata.size = req.file.size
+    rdata.type = req.file.mimetype
+  } else {
+    rdata = { "error": "No file Submitted" }
+  }
+
+  res.json(rdata)
+})
 
 
 
